3cedbd1c37f89f47eac734f741060307be967316
RstK <208619198+bowman025@users.noreply.github.com>
2026-05-04 13:24:05 +0200

    Scaffold the frontend

---
+++ apps/client/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': 'warn',
+  },
+};
