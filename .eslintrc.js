13e30d596bcc4c576950b99376bb0f9c56226f8c
RstK <208619198+bowman025@users.noreply.github.com>
2026-05-04 11:42:02 +0200

    Scaffold root directory

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+};
