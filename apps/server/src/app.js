0a82016c2072fb08d6fbe0d3ad7975124a478e88
RstK <208619198+bowman025@users.noreply.github.com>
2026-05-04 13:28:31 +0200

    Fix spelling error in server app file

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -13,7 +13,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  console.err(err);
+  console.error(err);
   res
     .status(err.status || 500)
     .json({ error: err.message || 'Internal server error.' });
