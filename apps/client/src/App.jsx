2c7252a9287e38bcb2958bd36b6b076323f9ca9c
RstK <208619198+bowman025@users.noreply.github.com>
2026-05-07 13:14:08 +0200

    Edit App.jsx and wire up routes

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,9 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import GamePage from './pages/GamePage/GamePage';
+import LeaderboardPage from './pages/LeaderboardPage/LeaderboardPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Home</div>} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/game/:imageId" element={<GamePage />} />
+      <Route path="/leaderboard/:imageId" element={<LeaderboardPage />} />
     </Routes>
   );
 }
